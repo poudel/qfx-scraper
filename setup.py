ab4c59ed944c511716fc34c6a3aa81c6357a2c5d
keshaB Paudel <self@keshab.net>
2018-04-14 19:28:25 +0545

    bumped version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://github.com/poudel/qfx-scraper'
 EMAIL = 'self@keshab.net'
 AUTHOR = 'keshaB Paudel'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "1.0.1"
+VERSION = "1.0.3"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
